e2b720e10d46c52a522eb56302b71beda1327ada
juplmn777 <julien.plumyoen@gmail.com>
2021-04-07 09:27:25 +0200

    circle defined

--- app.js
+++ app.js
@@ -3,3 +3,21 @@ const context = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+let x = 0;
+let y = 0;
+let turn = 0;
+
+//canvas center:
+context.translate(innerWidth / 2, innerHeight / 2);
+
+const drawn = () => {
+    
+
+  context.beginPath();
+  context.strokeStyle = 'purple';
+  context.arc(x, y, 100, 0, 2 * Math.PI);
+  context.stroke();
+};
+
+drawn();
